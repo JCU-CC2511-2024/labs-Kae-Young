b5fd91e4bd479958787b0676a28f9ff7fa52df00
Kae-Young <kaeyoung1@outlook.com>
2024-10-11 17:37:50 +1000

    create assignment2

---
+++ Assignment2/main.c
@@ -0,0 +1,16 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 11-Oct-2024 kaeyo
+ * Assignment2
+ * ***********************************************************/
+
+#include "pico/stdlib.h"
+#include <stdbool.h>
+#include "hardware/pwm.h"
+
+int main(void) {
+  // TODO - Initialise components and variables
+  while (true) {
+    // TODO - Repeated code here
+  }
+}
