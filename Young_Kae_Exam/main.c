95141c16b470ceb7eba7a64358747bc267b33400
Kae-Young <kaeyoung1@outlook.com>
2024-11-03 20:04:11 +1000

    add practice exam and actual exam files

---
+++ Young_Kae_Exam/main.c
@@ -0,0 +1,16 @@
+/**************************************************************
+ * main.c
+ * rev 1.0 03-Nov-2024 kaeyo
+ * Young_Kae_Exam
+ * ***********************************************************/
+
+#include "pico/stdlib.h"
+#include <stdbool.h>
+
+
+int main(void) {
+  // TODO - Initialise components and variables
+  while (true) {
+    // TODO - Repeated code here
+  }
+}
